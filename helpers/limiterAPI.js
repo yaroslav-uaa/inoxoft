63f118cd77bd6da4de85306a28414decd643f46c
Yaroslav <67288386+yaroslav-uaa@users.noreply.github.com>
2021-09-17 01:30:23 +0300

    fix bug in documentation

--- helpers/limiterAPI.js
+++ helpers/limiterAPI.js
@@ -2,10 +2,10 @@ const { statusCodesEnum } = require('../config');
 
 const limiterAPI = {
     windowMs: 15 * 60 * 1000,
-    max: 1000,
-    handler: (req, res) => res
-        .status(statusCodesEnum.TOO_MANY_REQUESTS)
-        .json({ message: 'Too many requests' }),
+    max: 10000,
+    handler: (req, res) => res.status(statusCodesEnum.TOO_MANY_REQUESTS).json({
+        message: 'Too many requests',
+    }),
 };
 
 module.exports = limiterAPI;
