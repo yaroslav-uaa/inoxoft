7aed7f3d50ef2eeb7e0c1eab64564855c6282079
Yaroslav <67288386+yaroslav-uaa@users.noreply.github.com>
2021-10-24 18:18:42 +0300

    fix cors

--- config/configs.js
+++ config/configs.js
@@ -18,7 +18,7 @@ module.exports = {
 
     ALLOWED_ORIGIN:
         process.env.ALLOWED_ORIGIN ||
-        'http://localhost:4200;http://localhost:3000;http://localhost:5000',
+        'https://four-this-gen.vercel.app;http://localhost:4200;http://localhost:3000;http://localhost:5000;',
 
     SENTRY_DSN: process.env.SENTRY_DSN,
 };
